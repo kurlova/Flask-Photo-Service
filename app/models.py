2244b2648aec381f6d1f204314d17846d9f7d408
Yelena <sigizmoon@list.ru>
2016-04-13 23:04:14 +0500

    Added Course.img field to the database. Changed paths in index.html

--- app/models.py
+++ app/models.py
@@ -24,3 +24,4 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500), nullable=False)
+    img = db.Column(db.String(128), nullable=True)
