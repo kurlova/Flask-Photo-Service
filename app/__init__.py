04e39c684d5eff4ffd3670904881abc1687c42b0
Yelena <sigizmoon@list.ru>
2016-04-13 22:33:27 +0500

    New changelist

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='static')
 app.config.from_object('config')
 db = SQLAlchemy(app)
 lm = LoginManager(app)
 lm.login_view = 'index'
+
+db.create_all()
+
+from app.models import User
+
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(User, methods=['GET', 'POST', 'DELETE', 'PUT'],
+                       collection_name='app')
 
 from app import views, models
