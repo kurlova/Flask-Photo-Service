00ab4085c1dc1296c9042962f93ddf813c14497f
Yelena <sigizmoon@list.ru>
2016-04-16 01:08:51 +0500

    Organization class, Type class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from flask.ext.login import LoginManager, UserMixin
+from flask.ext.login import UserMixin
 from app import db, lm
 
 
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), nullable=True)
     country = db.Column(db.String(64), nullable=True)
     city = db.Column(db.String(64), nullable=True)
+    org_name = db.Column(db.Integer, db.ForeignKey('organizations.name'), nullable=True)
 
     def __repr__(self):
         return '<User number %r>' % self.id
@@ -19,9 +20,31 @@ class User(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
+
 class Course(db.Model):
     __tablename__ = 'coursers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500), nullable=False)
     img = db.Column(db.String(128), nullable=True)
+    org_name = db.Column(db.Integer, db.ForeignKey('organizations.name'))
+
+
+class Organization(db.Model):
+    __tablename__ = 'organizations'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True)
+    website = db.Column(db.String(64), nullable=True)
+    country = db.Column(db.String(64), nullable=True)
+    city = db.Column(db.String(64), nullable=True)
+    creator = db.relationship('User', backref='creator', lazy='dynamic')
+    courses = db.relationship('Course', backref='courses', lazy='dynamic')
+    type = db.Column(db.String, db.ForeignKey('types.type'))
+
+
+class OrganizationType(db.Model):
+    __tablename__ = 'types'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(30))
+
+
