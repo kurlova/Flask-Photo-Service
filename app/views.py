f98c3dd0c79ef018bcc79085592de3c2281f8e1a
Yelena <sigizmoon@list.ru>
2016-04-19 00:44:37 +0500

    костыль с куками

--- app/views.py
+++ app/views.py
@@ -79,8 +79,6 @@ def logout():
     return response
 
 
-#When the user clicks the "Login in with ..." link to initiate an OAuth authentication
-# the following application route is invoked
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
     if not current_user.is_anonymous:
