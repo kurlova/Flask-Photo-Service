72c7a5b27b868ed417700f462f0c22f327a5d11b
Yelena <sigizmoon@list.ru>
2016-06-14 03:43:56 +0500

    для диплома

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('glavApp', ['ngRoute', 'ngCookies', 'mediaPlayer']);
+var app = angular.module('glavApp', ['ngRoute', 'ngCookies']);
 
 var myapp = function() {};
 
